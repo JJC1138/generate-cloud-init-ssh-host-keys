9d28758ca67bd2431a570a7d96b8f719b6fbe399
Jon Colverson <github@vcxz.co.uk>
2019-01-03 01:47:39 +0000

    Added creation and unconditional removal of a temporary folder to work in

--- generatecloudinitsshhostkeys/__main__.py
+++ generatecloudinitsshhostkeys/__main__.py
@@ -1,7 +1,9 @@
 import argparse
+import shutil
 import sys
+import tempfile
 
-def main():
+def run(temp_folder_name):
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('config_file_name', metavar='config-file-name')
     if len(sys.argv) == 1:
@@ -12,3 +14,10 @@ def main():
     
     with open(args.config_file_name, 'at') as config_file:
         config_file.write(config)
+
+def main():
+    temp_folder_name = tempfile.mkdtemp(prefix='generate-cloud-init-ssh-host-keys')
+    try:
+        run(temp_folder_name)
+    finally:
+        shutil.rmtree(temp_folder_name, ignore_errors=True)
