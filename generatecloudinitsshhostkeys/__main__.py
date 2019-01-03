ee69faed22ce3c80a597458df86b44d9fd6ef353
Jon Colverson <github@vcxz.co.uk>
2019-01-03 03:13:51 +0000

    Added generation of YAML output containing the keys

--- generatecloudinitsshhostkeys/__main__.py
+++ generatecloudinitsshhostkeys/__main__.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import yaml
 
 def run(temp_folder_name):
     def generate_keys(key_type):
@@ -10,19 +11,21 @@ def run(temp_folder_name):
         subprocess.check_call(['ssh-keygen', '-q', '-N', '', '-t', key_type, '-f', private_key_filename], stdout=sys.stderr, stderr=sys.stderr)
         public_key_filename = '%s.pub' % private_key_filename
         
-        with open(private_key_filename, 'rb') as private_key_file:
+        with open(private_key_filename, 'rt') as private_key_file:
             private_key = private_key_file.read()
-        with open(public_key_filename, 'rb') as public_key_file:
+        with open(public_key_filename, 'rt') as public_key_file:
             public_key = public_key_file.read()
         
         return (private_key, public_key)
     
+    keys = []
+    
     for key_type in ['ecdsa', 'ed25519', 'rsa']:
         (private_key, public_key) = generate_keys(key_type)
+        keys.append({('%s_private' % key_type): private_key})
+        keys.append({('%s_public' % key_type): public_key})
     
-    config = "test content\n" # FIXME remove
-    
-    sys.stdout.write(config)
+    yaml.dump({'ssh_keys': keys}, sys.stdout)
 
 def main():
     temp_folder_name = tempfile.mkdtemp(prefix='generate-cloud-init-ssh-host-keys')
