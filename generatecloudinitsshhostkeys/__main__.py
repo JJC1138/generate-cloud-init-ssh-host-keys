2b7382e3c30cbb1bdb6cb517dc521d429e758f2b
Jon Colverson <github@vcxz.co.uk>
2019-01-03 01:23:31 +0000

    Added command-line argument parsing

--- generatecloudinitsshhostkeys/__main__.py
+++ generatecloudinitsshhostkeys/__main__.py
@@ -1,2 +1,11 @@
+import argparse
+import sys
+
 def main():
-    print("Hi")
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('config_file_name', metavar='config-file-name')
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+    args = arg_parser.parse_args()
+    
+    print(args.config_file_name)
